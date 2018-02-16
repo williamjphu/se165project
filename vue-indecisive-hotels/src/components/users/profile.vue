a7ce51ce6ef20c1b6e0c89f6f216c38fff243785
Cameron Chien <KPMCameronC@gmail.com>
2018-02-15 21:25:40 -0800

    account details card for profile.vue

--- vue-indecisive-hotels/src/components/users/profile.vue
+++ vue-indecisive-hotels/src/components/users/profile.vue
@@ -1,6 +1,44 @@
 <template>
-  <v-layout>
-    <h1>USER PROFILE PAGE</h1>
-    <h2>THIS IS A STUB PAGE</h2>
+<v-container fluid>
+  <v-layout row>
+    <v-flex xs8 pa-5>
+      <v-card >
+        <v-toolbar color="orange">
+          
+          <v-toolbar-title>Account Details</v-toolbar-title>
+          <v-spacer></v-spacer>
+          </v-toolbar>
+        <v-list two-line>
+          <v-list-tile>
+          	<v-list-tile-title>Name: {name}
+          	</v-list-tile-title>
+          	<v-list-tile-subtitle>
+          		edit
+          	</v-list-tile-subtitle>
+        </v-list-tile>
+        </v-list>
+        <v-list two-line>
+          <v-list-tile>
+          	<v-list-tile-title>Email: {email}
+          	</v-list-tile-title>
+          	<v-list-tile-subtitle>
+          		edit
+          	</v-list-tile-subtitle>
+        </v-list-tile>
+        </v-list> 
+        <v-list two-line>
+          <v-list-tile>
+          	<v-list-tile-title>Password: {password}
+          	</v-list-tile-title>
+          	<v-list-tile-subtitle>
+          		edit
+          	</v-list-tile-subtitle>
+        </v-list-tile>
+        </v-list> 
+        
+      </v-card>
+    </v-flex>
   </v-layout>
+</v-container>
 </template>
+
