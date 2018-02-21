77ba0bd430da1be2de0f16ee7552208ab789d7bf
Cameron Chien <KPMCameronC@gmail.com>
2018-02-21 15:12:56 -0800

    button and dialogs added for editing name, email, and password

--- vue-indecisive-hotels/src/components/users/profile.vue
+++ vue-indecisive-hotels/src/components/users/profile.vue
@@ -1,4 +1,5 @@
 <template>
+<v-app>
 <v-container fluid>
   <v-layout row>
     <v-flex xs8 pa-5>
@@ -12,27 +13,99 @@
           <v-list-tile>
           	<v-list-tile-title>Name: {name}
           	</v-list-tile-title>
-          	<v-list-tile-subtitle>
-          		edit
-          	</v-list-tile-subtitle>
+          	 <v-dialog v-model="nameDialog" persistent max-width="500px">
+      <v-btn color="orange" dark slot="activator">Edit Name</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edit Name</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="New first name" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="New last name" hint="example of helper text only on focus"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="orange" flat @click.native="nameDialog = false">Close</v-btn>
+          <v-btn color="orange" flat @click.native="nameDialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
         </v-list-tile>
         </v-list>
         <v-list two-line>
           <v-list-tile>
           	<v-list-tile-title>Email: {email}
           	</v-list-tile-title>
-          	<v-list-tile-subtitle>
-          		edit
-          	</v-list-tile-subtitle>
+          	<v-dialog v-model="emailDialog" persistent max-width="500px">
+      <v-btn color="orange" dark slot="activator">Edit Email</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edit Email</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm12>
+                <v-text-field label="New email" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm12>
+                <v-text-field label="Confirm new email" ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+         
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="orange" flat @click.native="emailDialog = false">Close</v-btn>
+          <v-btn color="orange" flat @click.native="emailDialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
         </v-list-tile>
         </v-list> 
         <v-list two-line>
           <v-list-tile>
-          	<v-list-tile-title>Password: {password}
+          	<v-list-tile-title>Password: Set
           	</v-list-tile-title>
-          	<v-list-tile-subtitle>
-          		edit
-          	</v-list-tile-subtitle>
+          	<v-dialog v-model="passwordDialog" persistent max-width="500px">
+      <v-btn color="orange" dark slot="activator">Edit Password</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edit Password</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm12>
+                <v-text-field label="Current Password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm12>
+                <v-text-field label="New Password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm12>
+                <v-text-field label="Confirm New Password" ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+         
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="orange" flat @click.native="passwordDialog = false">Close</v-btn>
+          <v-btn color="orange" flat @click.native="passwordDialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
         </v-list-tile>
         </v-list> 
         
@@ -40,5 +113,14 @@
     </v-flex>
   </v-layout>
 </v-container>
+</v-app>
 </template>
-
+<script>
+  export default {
+    data: () => ({
+      nameDialog: false,
+      emailDialog: false,
+      passwordDialog: false
+    })
+  }
+</script>
