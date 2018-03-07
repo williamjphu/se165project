77f39e54e52f3a1cac24d8f33dc00321608d5b7c
Marcus Norona <marcusjeremynorona@gmail.com>
2018-03-07 12:39:33 -0800

    googleplaces fix

--- vue-indecisive-hotels/src/main.js
+++ vue-indecisive-hotels/src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import * as firebase from 'firebase'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
