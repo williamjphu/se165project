590eb2f36f15e8a1a59336d800858bd1003aebcf
Quang Duy Luong <qdluong.se@gmail.com>
2018-03-12 04:44:18 -0700

    Search card hotel prop, basic sorting
    Made search_card accept a hotel object prop so it renders data based on
    the object passed to it. Basic sorting by price, name, star-rating done;
    distance needs to wait for geolocation to be implemented. Tested on a
    set of four test hotels.

--- vue-indecisive-hotels/src/components/bookings/search_card.vue
+++ vue-indecisive-hotels/src/components/bookings/search_card.vue
@@ -47,7 +47,7 @@
                           inline
                           v-model="hotel.stars">
                         </vue-star-rating>
-                        {{ hotel.address }}
+                        {{ hotel.address }}, {{ hotel.city }}
                       </span>
                     </v-flex>
                     <v-flex xs12 py-0 v-if="!$vuetify.breakpoint.xsOnly">
@@ -85,7 +85,7 @@
                           <v-flex xs12>
                             <span class="subheading">
                               <v-chip label color="error" text-color="white">
-                                <v-icon left>label</v-icon>{{ discount }}% OFF TODAY
+                                <v-icon left>label</v-icon><span class="subheading">{{ discount }}% OFF</span>
                               </v-chip>
                             </span>
                           </v-flex>
@@ -114,23 +114,9 @@
     data () {
       return {
         // Any variable data should go in here
-        hotel: {
-          name: 'Hotel Indecisive',
-          address: '1 Washington Square',
-          city: 'San Jose',
-          state: 'CA',
-          country: 'United States',
-          stars: 4,
-          reviewsCount: 71,
-          reviewsAverage: 7.9,
-          originalRate: 519,
-          lowestRate: 129,
-          offers: ['Breakfast', 'Stuff', 'More stuff'],
-          options: ['Something', 'Something else', 'More options'],
-          images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
-        }
       }
     },
+    props: ['hotel'],
     computed: {
       discount () {
         return Math.round((this.hotel.originalRate - this.hotel.lowestRate) / (this.hotel.originalRate) * 100)
--- vue-indecisive-hotels/src/components/bookings/search_results.vue
+++ vue-indecisive-hotels/src/components/bookings/search_results.vue
@@ -11,7 +11,7 @@
           </v-flex>
           <v-flex xs12 md9>
             <v-card flat>
-              <search-card v-for="i in 6" :key="i"></search-card>
+              <search-card v-for="(hotel, i) in hotels" :key="i" :hotel="hotel"></search-card>
             </v-card>
           </v-flex>
         </v-layout>
@@ -27,10 +27,86 @@
     data () {
       return {
         // Any variable data should go in here
+        sort: 1,
+        hotels2: [
+          {
+            name: 'Independent Hotel',
+            address: '1 Washington Square',
+            city: 'San Jose',
+            state: 'CA',
+            country: 'United States',
+            stars: 4,
+            reviewsCount: 71,
+            reviewsAverage: 7.9,
+            originalRate: 519,
+            lowestRate: 129,
+            offers: ['Breakfast', 'Stuff', 'More stuff'],
+            options: ['Something', 'Something else', 'More options'],
+            images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+          },
+          {
+            name: 'Hotel Indecisive 2',
+            address: '188 Moore Drive',
+            city: 'Santa Clara',
+            state: 'CA',
+            country: 'United States',
+            stars: 5,
+            reviewsCount: 1990,
+            reviewsAverage: 8.9,
+            originalRate: 989,
+            lowestRate: 449,
+            offers: ['Breakfast', 'More stuff'],
+            options: ['Something', 'More options'],
+            images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+          },
+          {
+            name: 'Some other hotel',
+            address: '1729 Sycamore Lake Rd',
+            city: 'Houston',
+            state: 'TX',
+            country: 'United States',
+            stars: 3,
+            reviewsCount: 171,
+            reviewsAverage: 6.3,
+            originalRate: 299,
+            lowestRate: 89,
+            offers: [],
+            options: ['Something', 'Something else', 'More options'],
+            images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+          },
+          {
+            name: 'Hotel Imperial',
+            address: '1223 Imperial St',
+            city: 'Riverdale',
+            state: 'NY',
+            country: 'United States',
+            stars: 4,
+            reviewsCount: 271,
+            reviewsAverage: 7.3,
+            originalRate: 489,
+            lowestRate: 229,
+            offers: ['Breakfast', 'Stuff', 'More stuff'],
+            options: ['Something', 'Something else', 'More options'],
+            images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+          }
+        ]
       }
     },
     computed: {
       // Any variables that need to be dynamically recomputed go in here
+      hotels () {
+        const sort = this.sort
+        var hotels = this.hotels2
+        if (sort === 0) {
+          return hotels.sort((a, b) => a.lowestRate - b.lowestRate)
+        } else if (sort === 1) {
+          return hotels.sort((a, b) => a.name.localeCompare(b.name))
+        } else if (sort === 2) {
+          return hotels.sort((a, b) => a.stars - b.stars)
+        } else {
+          return hotels
+        }
+      }
     },
     components: {
       'search-filter': searchFilter,
