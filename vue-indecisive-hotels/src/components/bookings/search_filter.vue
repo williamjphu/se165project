6636d33d641e205076c80586ce5d7829e368d953
Quang Duy Luong <qdluong.se@gmail.com>
2018-03-13 00:34:04 -0700

    Sorting buttons working, scaffolds for live-chat

--- vue-indecisive-hotels/src/components/bookings/search_filter.vue
+++ vue-indecisive-hotels/src/components/bookings/search_filter.vue
@@ -4,15 +4,22 @@
       <v-flex d-flex xs12>
         <v-expansion-panel expand>
           <v-expansion-panel-content>
-            <div slot="header">Sort</div>
-            <v-card>
-              <v-card-text class="grey lighten-4">By price, from low to high</v-card-text>
-              <v-card-text class="grey lighten-3">By price, from high to low</v-card-text>
-              <v-card-text class="grey lighten-4">By rating, from low to high</v-card-text>
-              <v-card-text class="grey lighten-3">By rating, from high to low</v-card-text>
-              <v-card-text class="grey lighten-4">Alphabetically, from low to high</v-card-text>
-              <v-card-text class="grey lighten-3">Alphabetically, from high to low</v-card-text>
-            </v-card>
+            <div slot="header">{{ sortMethod }}</div>
+            <v-btn color="grey lighten-4" block @click="setSort('A to Z')">
+              A to Z
+            </v-btn>
+            <v-btn color="grey lighten-4" block @click="setSort('Z to A')">
+              Z to A
+            </v-btn>
+            <v-btn color="grey lighten-4" block @click="setSort('Cheapest')">
+              Cheapest
+            </v-btn>
+            <v-btn color="grey lighten-4" block @click="setSort('Most expensive')">
+              Most expensive
+            </v-btn>
+            <v-btn color="grey lighten-4" block @click="setSort('Most stars')">
+              Most stars
+            </v-btn>
           </v-expansion-panel-content>
           <v-expansion-panel-content>
             <div slot="header">Filter By</div>
@@ -25,3 +32,19 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sortMethod: 'Sort by'
+      }
+    },
+    methods: {
+      setSort (payload) {
+        this.sortMethod = 'Sort from ' + payload
+        this.$store.commit('setSort', payload)
+      }
+    }
+  }
+</script>
--- vue-indecisive-hotels/src/store/modules/hotels.js
+++ vue-indecisive-hotels/src/store/modules/hotels.js
@@ -5,7 +5,70 @@ const state = {
   longitude: null,
   latitude: null,
   locationLoading: false,
-  locationError: null
+  locationError: null,
+  sort: '',
+  hotels: [
+    {
+      name: 'Independent Hotel',
+      address: '1 Washington Square',
+      city: 'San Jose',
+      state: 'CA',
+      country: 'United States',
+      stars: 4,
+      reviewsCount: 71,
+      reviewsAverage: 7.9,
+      originalRate: 519,
+      lowestRate: 129,
+      offers: ['Breakfast', 'Stuff', 'More stuff'],
+      options: ['Something', 'Something else', 'More options'],
+      images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+    },
+    {
+      name: 'Hotel Indecisive 2',
+      address: '188 Moore Drive',
+      city: 'Santa Clara',
+      state: 'CA',
+      country: 'United States',
+      stars: 5,
+      reviewsCount: 1990,
+      reviewsAverage: 8.9,
+      originalRate: 989,
+      lowestRate: 449,
+      offers: ['Breakfast', 'More stuff'],
+      options: ['Something', 'More options'],
+      images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+    },
+    {
+      name: 'Some other hotel',
+      address: '1729 Sycamore Lake Rd',
+      city: 'Houston',
+      state: 'TX',
+      country: 'United States',
+      stars: 3,
+      reviewsCount: 171,
+      reviewsAverage: 6.3,
+      originalRate: 299,
+      lowestRate: 89,
+      offers: [],
+      options: ['Something', 'Something else', 'More options'],
+      images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+    },
+    {
+      name: 'Hotel Imperial',
+      address: '1223 Imperial St',
+      city: 'Riverdale',
+      state: 'NY',
+      country: 'United States',
+      stars: 4,
+      reviewsCount: 271,
+      reviewsAverage: 7.3,
+      originalRate: 489,
+      lowestRate: 229,
+      offers: ['Breakfast', 'Stuff', 'More stuff'],
+      options: ['Something', 'Something else', 'More options'],
+      images: ['https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg', 'https://az760333.vo.msecnd.net/-/media/property/jdv/vitale/vitale-guestroom-king-03-pd0912-crpd1600x760.jpg?ts=93e9e6c4-b97c-4307-93c4-443dda035aa5', 'https://www.rwsentosa.com/-/media/project/non-gaming/rwsentosa/hotels/hard-rock-hotel-singapore/hardrockhotelsg-exterior.jpg?mh=666&la=en&h=666&w=1366&mw=1366&hash=7890A13D2C4FE83769F4A571B997007215155027', 'https://phgcdn.com/images/uploads/PARBH/corporatemasthead/hotel-balzac.jpg', 'https://www.corinthia.com/application/files/6315/0460/7084/corinthia-hotel-tripoli-lobby.jpg']
+    }
+  ]
 }
 
 const getters = {
@@ -20,6 +83,12 @@ const getters = {
   },
   locationError (state) {
     return state.locationError
+  },
+  getHotels (state) {
+    return state.hotels
+  },
+  getSort (state) {
+    return state.sort
   }
 }
 
@@ -38,6 +107,9 @@ const mutations = {
   },
   clearLocationError (state, payload) {
     state.error = null
+  },
+  setSort (state, payload) {
+    state.sort = payload
   }
 }
 
@@ -47,7 +119,7 @@ const actions = {
     var assert = require('assert')
     var googlePlacesReq = require('googleplaces')
     console.log('required')
-    //
+    console.log(cors())
     var googlePlacesInstance = new googleplaces('AIzaSyB_QVILHYo25sC2lWgN2RWXswo8LTFu-ME','json')
     console.log(googlePlacesInstance)
     var parameters = {
