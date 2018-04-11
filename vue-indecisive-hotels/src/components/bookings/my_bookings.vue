7c96ce78ff421a58c5b390c32f6cd5089b55fdf3
Cameron Chien <KPMCameronC@gmail.com>
2018-04-11 16:11:22 -0700

    myBookings page using real data

--- vue-indecisive-hotels/src/components/bookings/my_bookings.vue
+++ vue-indecisive-hotels/src/components/bookings/my_bookings.vue
@@ -11,20 +11,20 @@
             <v-layout row wrap>
              <v-flex pa-4
 
-          v-for="n in 3"
-          :key="n"
+          v-for="(booking, i) in bookings"
+          :key="i"
         >
             <v-layout>
             <v-card >
-        <v-card-media src="https://imageserver-bisnow1.netdna-ssl.com/r6Lny5c7mIylpgQRrk1c5_xsk5Q=/0x0/publisher/59050a4778517_Screen_Shot_2017_04_29_at_5_48_14_PM.png" height="250px">
+        <v-card-media :src="booking.bookingDetails.photo" height="250px">
         </v-card-media>
         <v-card-title primary-title>
           <div>
-            <h3 class="headline mb-0">{{booking.hotel}}</h3>
-            <div>{{booking.location}} </div>
+            <h3 class="headline mb-0">{{booking.bookingDetails.name}}</h3>
+            <div>{{booking.address}} </div>
             <br>
-            <div> Check In: {{booking.startDate}} </div>
-            <div> Check Out: {{booking.endDate}} </div>
+            <div> Check In: {{booking.bookingDetails.dateIn}} </div>
+            <div> Check Out: {{booking.bookingDetails.dateOut}} </div>
           </div>
         </v-card-title>
         <v-card-actions>
@@ -44,19 +44,19 @@
             <v-layout row wrap>
              <v-flex pa-4
 
-          v-for="n in 6"
-          :key="n"
+          v-for="(booking, i) in bookings"
+          :key="i"
         >
             
             <v-card>
-        <v-card-media src="https://www.islands.com/sites/islands.com/files/styles/1000_1x_/public/images/2016/11/atlantis-bahamas-greek-10.jpg?itok=qlOLtdq9" height="250px">
+        <v-card-media :src="booking.bookingDetails.photo" height="250px">
         </v-card-media>
         <v-card-title primary-title>
           <div>
-            <h3 class="headline mb-0">{{booking.hotel}}</h3>
-            <div>{{booking.location}} </div>
+            <h3 class="headline mb-0">{{booking.bookingDetails.name}}</h3>
+            <div>{{booking.bookingDetails.address}} </div>
             <br>
-            <div> Trip Dates: {{booking.startDate}} to {{booking.endDate}} </div>
+            <div> Trip Dates: {{booking.bookingDetails.dateIn}} to {{booking.bookingDetails.dateOut}} </div>
           </div>
         </v-card-title>
         <v-card-actions>
@@ -79,21 +79,29 @@
 
 <script>
   export default {
-    data () {
-      return {
-        booking: {
-          hotel: 'Cameron\'s Hotel',
-          location: 'Noord, Aruba',
-          startDate: 'Mar 21',
-          endDate: 'Mar 24',
-          picture: 'https://imageserver-bisnow1.netdna-ssl.com/r6Lny5c7mIylpgQRrk1c5_xsk5Q=/0x0/publisher/59050a4778517_Screen_Shot_2017_04_29_at_5_48_14_PM.png',
-          picture2: 'https://res.cloudinary.com/lastminute/image/upload/c_scale,w_630/v1430409557/SWIMMING_POOL_SP_NH_nice_019_hutqjx.jpg'
-        }
 
+    // data () {
+    //   booking:
+    //   return {
+    //     booking: {
+    //       hotel: 'Cameron\'s Hotel',
+    //       location: 'Noord, Aruba',
+    //       startDate: 'Mar 21',
+    //       endDate: 'Mar 24',
+    //       picture: 'https://imageserver-bisnow1.netdna-ssl.com/r6Lny5c7mIylpgQRrk1c5_xsk5Q=/0x0/publisher/59050a4778517_Screen_Shot_2017_04_29_at_5_48_14_PM.png',
+    //       picture2: 'https://res.cloudinary.com/lastminute/image/upload/c_scale,w_630/v1430409557/SWIMMING_POOL_SP_NH_nice_019_hutqjx.jpg'
+    //     }
+
+      // }
+    // },
+    computed: {
+      bookings () {
+        return this.$store.getters.bookings
       }
     },
-    computed: {
-      // Any variables that need to be dynamically recomputed go in here
-    }
+  methods: {
+
   }
+}
+  
 </script>
