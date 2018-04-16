ca237f88c10ac7f4d2302ed86ea10ebd0a08356b
KungPaoMushu <kpmcameronc@gmail.com>
2018-04-15 21:57:03 -0700

    Display booking based on status

--- vue-indecisive-hotels/src/components/bookings/my_bookings.vue
+++ vue-indecisive-hotels/src/components/bookings/my_bookings.vue
@@ -3,9 +3,7 @@
     <section>
       <v-container d-flex class="text-xs-center" style="text-shadow: 1px 1px #111111;">
         <v-layout column align-center>
-          <h3 class="display-2">Cameron :)</h3>
-          <br><br><br>
-
+           <br>
           <v-flex xs12>
             <h3 class="display-2">Upcoming Bookings</h3>
             <v-layout row wrap>
@@ -14,22 +12,22 @@
           v-for="(booking, i) in bookings"
           :key="i"
         >
-        <v-layout>
-           <!--  <v-layout v-if="booking.bookingDetails.dateOut < "> -->
+        
+            <v-layout v-if="booking.status == 'pending' "> 
             <v-card >
         <v-card-media :src="booking.bookingDetails.photo" height="250px">
         </v-card-media>
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">{{booking.bookingDetails.name}}</h3>
-            <div>{{booking.address}} </div>
+            <div>{{booking.bookingDetails.address}} </div>
             <br>
             <div> Check In: {{booking.bookingDetails.dateIn}} </div>
             <div> Check Out: {{booking.bookingDetails.dateOut}} </div>
           </div>
         </v-card-title>
         <v-card-actions>
-          <v-btn flat color="brown darken-2" @click = "bookingInformation" >View Details</v-btn>
+          <v-btn flat color="brown darken-2" @click = "bookingInformation" >Hotel Details</v-btn>
           <v-btn flat color="brown darken-2">Modify</v-btn>
         </v-card-actions>
       </v-card>
@@ -49,7 +47,7 @@
           :key="i"
         >
             
-            <v-card>
+            <v-card  v-if="booking.status != 'pending' ">
         <v-card-media :src="booking.bookingDetails.photo" height="250px">
         </v-card-media>
         <v-card-title primary-title>
@@ -61,7 +59,7 @@
           </div>
         </v-card-title>
         <v-card-actions>
-          <v-btn @click = 'bookingInformation' flat color="brown darken-2">View Details</v-btn>
+          <v-btn @click = 'bookingInformation' flat color="brown darken-2">Hotel Details</v-btn>
         </v-card-actions>
       </v-card>
     
@@ -84,21 +82,7 @@
       var time = new Date()
       return time
     },
-
-    // data () {
-    //   booking:
-    //   return {
-    //     booking: {
-    //       hotel: 'Cameron\'s Hotel',
-    //       location: 'Noord, Aruba',
-    //       startDate: 'Mar 21',
-    //       endDate: 'Mar 24',
-    //       picture: 'https://imageserver-bisnow1.netdna-ssl.com/r6Lny5c7mIylpgQRrk1c5_xsk5Q=/0x0/publisher/59050a4778517_Screen_Shot_2017_04_29_at_5_48_14_PM.png',
-    //       picture2: 'https://res.cloudinary.com/lastminute/image/upload/c_scale,w_630/v1430409557/SWIMMING_POOL_SP_NH_nice_019_hutqjx.jpg'
-    //     }
-
-      // }
-    // },
+    props: ['hotel'],
     computed: {
       bookings () {
         return this.$store.getters.bookings
@@ -108,7 +92,10 @@
     bookingInformation() {
       this.$router.replace('/bookinginformation')
       //need to pass booking object
-    }
+    },
+    onDetailsClicked (event) {
+        this.$emit('hotelSelected', this.hotel)
+      }
   }
 }
   
