aafd3dfc430486504a0521bef556be2e845d748b
Quang Duy (David) Luong <qdluong.se@gmail.com>
2018-04-15 18:22:07 -0700

    Reward points update

--- vue-indecisive-hotels/src/components/core/reward_points.vue
+++ vue-indecisive-hotels/src/components/core/reward_points.vue
@@ -1,7 +1,10 @@
 <template>
   <v-container fluid>
     <v-layout row wrap>
-      <v-flex xs12>
+      <v-flex xs12 md4>
+        Collect 1 point per night! For every 10 points, get 1 night free!
+      </v-flex>
+      <v-flex xs12 md4 d-flex>
         <v-progress-circular
           :size="150"
           :width="17"
@@ -11,6 +14,9 @@
         >
           <span class="display-1">{{ value/10 }}</span>
         </v-progress-circular>
+      </v-flex>
+      <v-flex xs12 md4>
+        You currently have {{ ~~(points/10) }} free night{{ ~~(points/10) == 1 ? '' : 's'}}. Collect {{ 10 - (points % 10) }} more point{{ points % 10 == 9 ? '' : 's' }} to earn another free night!
       </v-flex>
     </v-layout>
   </v-container>
@@ -31,9 +37,9 @@
     mounted () {
       this.interval = setInterval(() => {
         if (this.value < (this.points % 10) * 10) {
-          this.value += 10
+          this.value = (this.points % 10) * 10
         }
-      }, 500)
+      }, 200)
     }
   }
 </script>
